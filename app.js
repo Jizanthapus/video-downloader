58d54b9021d60797b5fd395ffa3849450c62ee3f
Alex <zander5391@gmail.com>
2020-11-03 11:30:40 -0500

    move to  main

--- app.js
+++ app.js
@@ -6,70 +6,74 @@ const Axios = require('axios').default
 const ProgressBar = require('progress')
 const Url = require('url');
 const parse = require('csv-parse/lib/sync');
+async function main() {
+  // get args
+  var myArgs = process.argv.slice(2);
+  if (myArgs.length < 1) {
+    throw 'NotEnoughArgs'
+  }
 
-// get args
-var myArgs = process.argv.slice(2);
-if (myArgs.length < 1) {
-  throw 'NotEnoughArgs'
-}
+  // get time
+  const now = Date.now()
 
-// get time
-const now = Date.now()
-
-// csv parser
-const linkFile = Fs.readFileSync(__dirname+'/links.csv')
-const episodes = parse(linkFile, {
-  columns: true,
-  skip_empty_lines: true
-})
-// console.log(episodes)
-
-for (var ep of episodes) {
-  console.log(ep['URL'])
-  const epUrl = new URL(ep['URL'])
-
-  // make sure link is current
-  const expiry = new Date(epUrl.searchParams.get('expires') * 1000)
-  console.log(expiry)
-  if (now > expiry) throw 'LinkIsOld' // TODO make new csv for files with old links
-
-  // get file name
-  var epFile = epUrl.pathname.substring(epUrl.pathname.lastIndexOf('/') + 1)
-  console.log(epFile)
-  var fileType = epFile.substring(epFile.lastIndexOf('.') + 1)
-  console.log(fileType)
-}
-
-async function downloadImage () {  
-  const url = new URL('https://cdn7.cloud9xx.com/user1342/ffac3a84fe7ad0e83052eb9724d0328f/EP.1.720p.mp4?token=n0Pp6FKUkhzNxV7u_0yp-w&expires=1604395489&id=78966');
-
-  console.log('Connecting …')
-  const { data, headers } = await Axios({
-    url: url.toString(),
-    method: 'GET',
-    responseType: 'stream'
+  // csv parser
+  const linkFile = Fs.readFileSync(__dirname+'/links.csv')
+  const episodes = parse(linkFile, {
+    columns: true,
+    skip_empty_lines: true
   })
-  const totalLength = headers['content-length']
+  // console.log(episodes)
 
-  console.log('Starting download')
-  const sizeInMB = sizeInMB/Math.pow(10, 6)
-  const sizeString = parseFloat(sizeInMB.toFixed(2), 10)
-  console.log(`Content length: ${sizeString} MB`)
-  const progressBar = new ProgressBar('-> downloading [:bar] :percent :etas', {
-    width: 40,
-    complete: '=',
-    incomplete: ' ',
-    renderThrottle: 1,
-    total: parseInt(totalLength)
-  })
+  for (var ep of episodes) {
+    console.log(ep['URL'])
+    const epUrl = new URL(ep['URL'])
 
-  const writer = Fs.createWriteStream(
-    Path.resolve(__dirname, 'downloads', 'Mobile Suit Gundam', 'Mobile Suit Gundam s01ep01.mp4')
-  )
+    // make sure link is current
+    const expiry = new Date(epUrl.searchParams.get('expires') * 1000)
+    console.log(expiry)
+    if (now > expiry) throw 'LinkIsOld' // TODO make new csv for files with old links
 
-  data.on('data', (chunk) => progressBar.tick(chunk.length))
-  data.pipe(writer)
+    // get file name
+    var epFile = epUrl.pathname.substring(epUrl.pathname.lastIndexOf('/') + 1)
+    console.log(epFile)
+    var fileType = epFile.substring(epFile.lastIndexOf('.') + 1)
+    console.log(fileType)
+  }
+
+  async function downloadImage () {  
+    const url = new URL('https://cdn7.cloud9xx.com/user1342/ffac3a84fe7ad0e83052eb9724d0328f/EP.1.720p.mp4?token=n0Pp6FKUkhzNxV7u_0yp-w&expires=1604395489&id=78966');
+
+    console.log('Connecting …')
+    const { data, headers } = await Axios({
+      url: url.toString(),
+      method: 'GET',
+      responseType: 'stream'
+    })
+    const totalLength = headers['content-length']
+
+    console.log('Starting download')
+    const sizeInMB = sizeInMB/Math.pow(10, 6)
+    const sizeString = parseFloat(sizeInMB.toFixed(2), 10)
+    console.log(`Content length: ${sizeString} MB`)
+    const progressBar = new ProgressBar('-> downloading [:bar] :percent :etas', {
+      width: 40,
+      complete: '=',
+      incomplete: ' ',
+      renderThrottle: 1,
+      total: parseInt(totalLength)
+    })
+
+    const writer = Fs.createWriteStream(
+      Path.resolve(__dirname, 'downloads', 'Mobile Suit Gundam', 'Mobile Suit Gundam s01ep01.mp4')
+    )
+
+    data.on('data', (chunk) => progressBar.tick(chunk.length))
+    data.pipe(writer)
+  }
+
+  // downloadImage()
 }
 
-// downloadImage()
+main()
+
 
